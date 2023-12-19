6649dd3d41da6a01a12bb37a75fecffdb3b69a15
ochiengowino <benjaminochieng99@gmail.com>
2023-12-19 15:34:06 +0300

    fetched web service data

--- Controllers/ApiTransactionsController.cs
+++ Controllers/ApiTransactionsController.cs
@@ -7,12 +7,16 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Text.Json;
+using Newtonsoft.Json;
 //using System.Net.Http.Formatting;
 using MobiAPI.Context;
 using MobiAPI.Models;
+
 using System.Web;
 using System.Web.Services;
 using System.Web.Services.Protocols;
+using System.Threading.Tasks;
 //using ServiceReference1;
 using ServiceReference2;
 using ServiceReference3;
@@ -25,14 +29,52 @@ namespace MobiAPI.Controllers
     public class ApiTransactionsController : ControllerBase
     {
         private readonly ApiContext _context;
+        private readonly HttpClient _httpClient;
 
-        public ApiTransactionsController(ApiContext context)
+        public ApiTransactionsController()
         {
             //MsaccoMobiAPI msaccoMobiAPI = new MsaccoMobiAPI();
             
-            _context = context;
+            //_context = context;
+
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://OchiengOwino:3332/CapitalSaccoInstance/");
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // Set Basic Authentication Header (replace 'username' and 'password' with your NAV credentials)
+            string username = "ochiengowinoben";
+            string password = "D3271n3d4gr87n322";
+            string base64Credentials = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($"{username}:{password}"));
+            // _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64Credentials);
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64Credentials);
         }
 
+        public class LoanApplication
+        {
+            //public string ODataContext { get; set; }
+            public List<ValueItem> Value { get; set; }
+        }
+
+        public class ValueItem
+        {
+            //public string ODataEtag { get; set; }
+            public string Loan_No { get; set; }
+            public DateTime Application_Date { get; set; }
+            public string Loan_Product_Type { get; set; }
+            public string Loan_Product_Type_Name { get; set; }
+            public string Member_No { get; set; }
+            public string Member_Name { get; set; }
+            public decimal Requested_Amount { get; set; }
+            public decimal Approved_Amount { get; set; }
+            public int Interest { get; set; }
+            public string Status { get; set; }
+            public string RecID { get; set; }
+            public string Captured_By { get; set; }
+            public string Global_Dimension_1_Code { get; set; }
+            public string Global_Dimension_2_Code { get; set; }
+            public string Staff_No { get; set; }
+        }
         // Create
         [HttpPost]
         public IActionResult Create(Transactions customerTransactions)
@@ -73,18 +115,16 @@ namespace MobiAPI.Controllers
 
             //MsaccoMobiAPI result = new MsaccoMobiAPI();
             LoanApplicationList webService = new LoanApplicationList();
-           /* NetworkCredential cred = new NetworkCredential(Globals.WebServiceUsername, Globals.WebServicePassword, Globals.WebServiceDomain);
-            webService.Timeout = Globals.Timeout;
-            webService.Url = Globals.Url;
-            webService.UseDefaultCredentials = false; // <-----
-            webService.Credentials = cred;*/
+        
 
             // Error/Failure - 400
             if (webService == null)
             {
                 return new JsonResult(NotFound());
             }
-            webService.Application_Date = DateTime.Parse("11/29/2021");
+        
+            //webService.Application_Date = DateTime.Parse("11/29/2021");
+           
 
             // Ok/Success - 200
             return new JsonResult(Ok(webService));
@@ -99,6 +139,53 @@ namespace MobiAPI.Controllers
 
             return new JsonResult(Ok(result));
         }
-        
+
+        [HttpGet("navisiondata")]
+        public async Task<IActionResult> GetNavisionData()
+        {
+            try
+            {
+                //HttpResponseMessage response = await _httpClient.GetAsync("http://OchiengOwino:3332/CapitalSaccoInstance/WS/CAPITAL%20SACCO/Page/LoanApplicationList");
+                HttpResponseMessage response = await _httpClient.GetAsync("http://OchiengOwino:3333/CapitalSaccoInstance/ODataV4/Company('CAPITAL%20SACCO')/LoanApplicationList");
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+
+                    /*string jsonString = Newtonsoft.Json.JsonSerializer.Serialize(result);
+                   return (IActionResult)JsonConvert.DeserializeObject<object>(jsonString);*/
+                    //return new JsonResult(Ok(jsonString)); // Returning response data
+                   // var jsonString = JsonConvert.SerializeObject(result);
+
+                    //var jsonString = JsonConvert.DeserializeObject<LoanApplication>(result);
+                    LoanApplication loanApplication = JsonConvert.DeserializeObject<LoanApplication>(result);
+                    return Ok(loanApplication);
+                }
+                else
+                {
+                    var failureState = new
+                    {
+                        MessageCode = "404",
+                        Message = "No data received"
+                    };
+
+                    return new JsonResult(NotFound(failureState));
+                    //return StatusCode((int)response.StatusCode, $"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Exception: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult PostNavData(LoanApplication loanApplication)
+        {
+
+            var data = loanApplication;
+            return Ok(data);
+            
+        }
+
     }
 }
